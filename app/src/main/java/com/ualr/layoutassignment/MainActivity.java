3928b0bca5ce6c6bb62d05705945b9bcd84258f9
MitchelCarreon <60196056+MitchelCarreon@users.noreply.github.com>
2021-10-28 23:31:13 -0500

    UI done??

--- app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ualr.layoutassignment.databinding.ActivityMainBinding;
+
+
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     // TODO 02. Create a new method called "calculateTotal" for calculating the invoice's total amount of money
+    public void calculateTotal(){
 
+    }
     // TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(this.binding.getRoot());
         super.onCreate(savedInstanceState);
     }
 }
