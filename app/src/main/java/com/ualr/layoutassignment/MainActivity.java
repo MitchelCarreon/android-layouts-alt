a3590af2439aead83c0475633601b4c75c9780db
MitchelCarreon <60196056+MitchelCarreon@users.noreply.github.com>
2021-10-29 21:03:54 -0500

    Clear button and changed calcTotal()
    iterates through layout, finds edittext, gets text, etc. etc.
    
    iterates through layout, finds checkboxes, checks if its checked, etc etc

--- app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -3,7 +3,7 @@ package com.ualr.layoutassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.Editable;
+
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -12,36 +12,40 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.ualr.layoutassignment.databinding.ActivityMainBinding;
 
-import org.w3c.dom.Text;
-
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
     public void calculateTotal() {
         Float sum = 0.0f;
+        Float discountRate = 0.5f;
 
-        LinearLayout cboxes = binding.prodCboxes;
+        LinearLayout cboxes = this.binding.prodCboxes;
         for (int i = 0; i < cboxes.getChildCount(); ++i){
             MaterialCheckBox curr_cbox = (MaterialCheckBox)cboxes.getChildAt(i);
             if (curr_cbox.isChecked()) {
-                TextInputEditText prod_field = (TextInputEditText) ((TextInputLayout) binding.prodFields.getChildAt(i)).getEditText();
+                TextInputEditText prod_field = (TextInputEditText) ((TextInputLayout) this.binding.prodFields.getChildAt(i)).getEditText();
 
                 assert prod_field != null;
                 String user_input = prod_field.getText().toString();
-                if (!user_input.matches("")) sum += Float.parseFloat(user_input);
+                if (user_input.matches("[0-9]+")) sum += Float.parseFloat(user_input);
             }
         }
+
+        if (this.binding.discountButton.isChecked()) sum -= (sum * discountRate);
 
         this.binding.totalDisp.setText(getResources().getString(R.string.dollar_symbol) + " " + sum.toString());
     }
 
-    public void clearInvoice(){
-        LinearLayout fields = binding.prodFields;
+    public void clearAll(){
+        LinearLayout fields = this.binding.prodFields;
         for (int i = 0; i < fields.getChildCount(); ++i) ((TextInputLayout) fields.getChildAt(i)).getEditText().setText("");
 
-        LinearLayout cboxes = binding.prodCboxes;
+        LinearLayout cboxes = this.binding.prodCboxes;
         for (int i = 0; i < cboxes.getChildCount(); ++i) ((MaterialCheckBox)cboxes.getChildAt(i)).setChecked(false);
+
+        this.binding.discountToggleGroup.clearChecked();
+        this.binding.totalDisp.setText(getResources().getString(R.string.money_disp_placeholder));
     }
 
     @Override
@@ -61,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         this.binding.resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                clearInvoice();
+                clearAll();
             }
         });
 
